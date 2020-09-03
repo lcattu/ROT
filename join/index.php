d7d7dc15cac722b534e09447fb39275aadba0c1e
Katsumasa Hiranuma <naru.naru.asu.naru@gmail.com>
2020-09-03 22:57:36 +0900

    会員登録機能HTML部分作成

--- join/index.php
+++ join/index.php
@@ -4,10 +4,38 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>会員登録</title>
-  
+
   <link rel="stylesheet" href="../style.css">
 </head>
 <body>
-  
+  <div id="head">
+    <h1>会員登録</h1>
+  </div>
+  <div id="content">
+    <p>次のフォームに必要事項をご記入ください</p>
+    <form action="" method="" enctype="multipqrt/form-data">
+    <dl>
+      <dt>ユーザー名<span></span></dt>
+      <dd>
+        <input type="text" name="name" size="35" maxlength="255" value="">
+
+      </dd>
+
+
+      <dt>メールアドレス</dt>
+      <dd>
+      <input type="text" name="email" size="35" maxlength="255" value="">
+      </dd>
+
+      <dt>パスワード</dt>
+      <dd>
+        <input type="password" name="password" size="10" maxlength="20" value="">
+      </dd>
+    </dl>
+    <div>
+      <input type="submit" value="入力内容を確認する">
+    </div>
+    </form>
+  </div>
 </body>
 </html>
