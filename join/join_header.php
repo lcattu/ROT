3b3fe423dd27fec5e3f3529997107c4e3f7f551f
Katsumasa Hiranuma <naru.naru.asu.naru@gmail.com>
2020-09-05 15:21:28 +0900

    会員登録時のヘッダー部分のHTML作成

---
+++ join/join_header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ROT</title>
+</head>
+<body>
+  <header>
+    <div class="header__left">
+     <img src="" alt="" class="header__left--logo">
+    </div>
+
+    <div class="header__right">
+      <div class="header__right--login">ログイン</div>
+    </div>
+
+  
+  </header>
+</body>
+</html>
