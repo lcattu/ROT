21e4e2f9d71557d9e0c62e8d339bd46900bae094
Katsumasa Hiranuma <naru.naru.asu.naru@gmail.com>
2020-09-06 15:55:46 +0900

    join/index.phpから join_header.php, footer.php, head.php への移管

--- join/index.php
+++ join/index.php
@@ -1,32 +1,7 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>ROT会員登録</title>
-
-  <link rel="stylesheet" href="../css/stylesheet.css">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
+<?php require "../head.php"; ?>
 <body>
-  <header class="l-header01">
-    <div class="l-headerWrapper">
-      <div class="l-headerWrapper-left">
-        <img src="../image/logo_test.jpg" alt="" class="d-none d-lg-block  p-logo__image-PC">
-
-
-        <img src="../image/logo_test.jpg" alt="" class="d-lg-none p-logo__image">
-      </div>
-
-    
-      <div class="l-headerWrapper-right">
-        <div class="c-login">
-          <a>ログイン</a>
-        </div>
-      </div>
-    </div>
-  </header>
+  <!-- ヘッダー -->
+  <?php require "join_header.php" ?>
 
 
 
@@ -60,4 +35,4 @@
     </form>
   </div>
 </body>
-</html>
+<?php require"../footer.php" ?>
--- join/join_header.php
+++ join/join_header.php
@@ -3,19 +3,31 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>ROT</title>
+  <title>ROT会員登録</title>
+
+  <link rel="stylesheet" href="../css/stylesheet.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-  <header>
-    <div class="header__left">
-     <img src="" alt="" class="header__left--logo">
-    </div>
+ <header class="l-header01">
+    <div class="l-headerWrapper">
+      <div class="l-headerWrapper-left">
+        <img src="../image/logo_test.jpg" alt="" class="d-none d-lg-block  p-logo__image-PC">
 
-    <div class="header__right">
-      <div class="header__right--login">ログイン</div>
-    </div>
 
-  
+        <img src="../image/logo_test.jpg" alt="" class="d-lg-none p-logo__image">
+      </div>
+
+    
+      <div class="l-headerWrapper-right">
+        <div class="c-login">
+          <a>ログイン</a>
+        </div>
+      </div>
+    </div>
   </header>
+
+
 </body>
-</html>
+</hteml>
