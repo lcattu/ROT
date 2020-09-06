f18f5317f6960be8507eb0679b7e49200a4dea34
Katsumasa Hiranuma <naru.naru.asu.naru@gmail.com>
2020-09-06 15:00:49 +0900

    joinヘッダーのレスポンシブ対応

--- join/index.php
+++ join/index.php
@@ -6,12 +6,17 @@
   <title>ROT会員登録</title>
 
   <link rel="stylesheet" href="../css/stylesheet.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
   <header class="l-header01">
     <div class="l-headerWrapper">
       <div class="l-headerWrapper-left">
-        <img src="../image/logo_test.jpg" alt="" class="p-logo__image">
+        <img src="../image/logo_test.jpg" alt="" class="d-none d-lg-block  p-logo__image-PC">
+
+
+        <img src="../image/logo_test.jpg" alt="" class="d-lg-none p-logo__image">
       </div>
 
     
