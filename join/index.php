35a5d7f6081fb4b4f6b04a05fa4a254d5a3a55b1
Katsumasa Hiranuma <naru.naru.asu.naru@gmail.com>
2020-09-12 01:07:40 +0900

    大枠の構造を更新

--- join/index.php
+++ join/index.php
@@ -3,6 +3,55 @@
   <!-- ヘッダー -->
   <?php require "join_header.php" ?>
 
+  <h1>Replication of Twitter</h1>
+
+
+<div class="p-welcomeWrapper">
+
+<!--#0 p-greetingModal -->
+<div class="c-modal p-greetingModal">
+  
+  <div class="c-welcomeWrapper">
+    <!-- #0 -->
+    <div class=" c-modal__body active">
+
+      <h3 class="c-modal__title">#0 Greeting</h3>
+      <p>はじめまして。カツノリです<br>Twitter風webアプリ完成しました。よろしかったら、遊んでみてください。</p> 
+    </div>
+    
+    <!-- #1 -->
+    <div class="c-modal__body">
+      <h3 class="c-modal__title">#1 プライバシーポリシー</h3>
+      <p>約束してちょ</p>
+      <input id="check" type="checkbox" >上記に同意する
+
+      </div>
+
+    <!-- #2 -->
+    <div class="c-modal__body">
+      <h3 class="c-modal__title">#2 登録画面</h3>
+      <p>登録ボタンを追加していく</p> 
+      </div>
+
+  </div>
+  
+
+  <!-- change-btn-wrapper -->
+  <div class="c-modal__footer">
+    <div class="change-btn prev-btn">←戻る</div>
+
+    
+    <div class="change-btn next-btn" >登録へ→</div>
+
+    <div class="alter-btn">登録へ→</div>
+
+
+
+    <div class="change-btn regi-btn">登録</div>
+  </div>
+</div>
+</div>
+
 
 
   <div id="head">
