43eb551b4807219ab8835ec41262d6568a3f94a6
Katsumasa Hiranuma <naru.naru.asu.naru@gmail.com>
2020-09-25 08:56:22 +0900

    thanks.phpの大雑把な設定完了

--- join/thanks.php
+++ join/thanks.php
@@ -5,14 +5,14 @@
 
   <h1>Replication of Twitter</h1>
 
+    <div class="p-welcomeWrapper">
+      <div class="c-modal">
+        
+        <h3 class="c-modal__title">#4 登録画面</h3>
+        <p>ユーザー登録が完了しました</p> 
+        <p><a href="../"></a>ログインする</p>
+      </div>
 
-  <div class="p-welcomeWrapper">
-
-    <h3 class="c-modal__title">#4 登録画面</h3>
-    <p>ユーザー登録が完了しました</p> 
-    <p><a href="../"></a>ログインする</p>
-
-
-  </div>
+    </div>
 </body>
 <?php require"../footer.php" ?>
