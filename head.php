ff7e46c4640d21b20c1d7a0a676283889141d116
Katsumasa Hiranuma <naru.naru.asu.naru@gmail.com>
2020-09-29 21:00:01 +0900

    CSSの読み込み順序、コメント追加

--- head.php
+++ head.php
@@ -6,11 +6,14 @@
   <meta name="description" content="ROTはtwitterを模したメッセージサービスです">
 
 
-  <link rel="stylesheet" href="../css/stylesheet.css">
+  <!-- Bootstrapの読み込み -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
+  <!-- fontawesome読み込み -->
   <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
 
+  <!-- CSSの読み込み -->
+  <link rel="stylesheet" href="../css/stylesheet.css">
 
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
