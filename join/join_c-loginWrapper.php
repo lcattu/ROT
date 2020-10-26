29466d93d8bff54c87f0794403dd493586681a1b
lcattu <naru.naru.asu.naru@gmail.com>
2020-10-26 21:43:05 +0900

    登録・ログイン時のモーダルの設定

--- join/join_c-loginWrapper.php
+++ join/join_c-loginWrapper.php
@@ -2,14 +2,57 @@
 session_start();
 ?>
 
-
+<!-- sample modal -->
 <div class="c-loginWrapper">
+
+  <!-- sample modal-wrap -->
   <div class="c-modalWrapper">
-    <div class="c-modal__regiBody">
-      <div class="p-closeBtn">
-        <i class="fa fa-2x fa-times"></i>
+
+    <!-- samplemodal-bg -->
+    <div class="c-modalWrapper__bg">&nbsp;</div>
+    
+    <!-- samplemodal-box -->
+    <div class=" c-modalWrapper__box">
+      
+      <!-- inner -->
+      <div class="c-modal__regiBody p-modal__body">
+        <div class="p-closeBtn">
+          <i class="fa fa-2x fa-times"></i>
+        </div>
+        <h4 class="p-modal__body--title">hogehoge</h4>
+        <form action="" method="post">
+          <dl>
+            <dt>メールアドレス</dt>
+            <dd>
+              <input type="text" name="email" size="35" maxlength="255" value="<?php print(htmlspecialchars($email , ENT_QUOTES)); ?>" />
+
+              <!-- エラーメッセージ -->
+              <?php if($error['login'] === 'blank'): ?>
+                <p class="">*メールアドレスとパスワードをご記入ください</p>
+              <?php endif; ?>
+
+              <?php if($error['login'] === 'failed'): ?>
+                <p class="">*ログインに失敗しました。正しくご記入ください</p>
+              <?php endif; ?>
+
+
+            </dd>
+            <dt>パスワード</dt>
+            <dd>
+              <input type="password" name="password" size="35" maxlength="255" value="<?php print(htmlspecialchars($_POST['password'] , ENT_QUOTES)); ?>" />
+            </dd>
+            <dt>ログイン情報の記録</dt>
+            <dd>
+              <input id="save" type="checkbox" name="save" value="on">
+              <label for="save">次回からは自動的にログインする</label>
+            </dd>
+          </dl>
+          <div>
+            <input type="submit" value="ログインする" />
+          </div>
+        </form>
       </div>
-      <h1>hogehoge</h1>
+
     </div>
   </div>
 </div>
--- join/join_c-welcomeWrapper.php
+++ join/join_c-welcomeWrapper.php
@@ -34,171 +34,169 @@ if(!empty($_POST)){
 
 
 
-echo '$keyの中身//<br>';
-var_dump($key);
-echo '<br>';
-
-echo '$error[$key]の中身//<br>';
-var_dump($error[$key]);
-
 ?>
-
+<!-- samplemodal -->
 <div class="c-welcomeWrapper">
-  <!--#0 p-greetingModal -->
+
+  <!-- samplemodal-wrap -->
   <div class="c-modalWrapper">
 
+    <!-- samplemodal-bg -->
+    <div class="c-modalWrapper__bg">&nbsp;</div>
+
+    <!-- samplemodal-box -->
+    <div class="c-modalWrapper__box">
     <div class="p-modal__btnWrapper">
-      <div class="p-closeBtn">
-        <i class="fa fa-2x fa-times"></i>
-      </div>
-
-      <div class="p-change-btn  p-prev-btn">
-        <div class="p-modal__turnIcon">
-          <i class ="fa fa-chevron-left fa-2x"></i>
-          <p>prev</p>
+        <div class="p-closeBtn">
+          <i class="fa fa-2x fa-times"></i>
         </div>
-      </div>
 
-      
-      <div class="p-change-btn p-next-btn " >
-        <div class="p-modal__turnIcon">
-          <i class="fa fa-chevron-right fa-2x"></i>
-          <p>next</p>
+        <div class="p-change-btn  p-prev-btn">
+          <div class="p-modal__turnIcon">
+            <i class ="fa fa-chevron-left fa-2x"></i>
+            <p>prev</p>
+          </div>
         </div>
+
         
-      </div>
-
-      <div class="p-alter-btn ">
-        <div class="p-modal__turnIcon">
-          <i class="fa fa-chevron-right fa-2x"></i>
-          next
-        </div>
-        
-      </div>
-    </div>
-
-
-    <!-- #0 -->
-    <div class=" c-modal__body p-modal__body">
-
-      <h3 class="p-modal__body--title">#0 Greeting</h3>
-      <p>はじめまして。カツノリです<br>Twitter風webアプリ完成しました。よろしかったら、遊んでみてください。</p> 
-    </div>
-  
-    <!-- #1 -->
-    <div class="c-modal__body p-modal__body">
-      <h3 class="p-modal__body--title">個人情報取扱指針</h3>
-        <div class="p-modal__body--txtArea">
-        
-          <?php include('privacy&policy.php'); ?>
-        </div>
-        <div class = p-modal__body--checkBtn>
-          <input id="check" type="checkbox" >上記に同意する
-        </div>
-    </div>
-<!-- △△△△△△△△ -->
-    <!-- #2 -->
-    <div class="c-modal__body  p-modal__body">
-      <h3 class="p-modal__body--title">#2 登録画面</h3>
-      <p>登録ボタンを追加していく</p> 
-      <form action="" method="post" enctype="multipart/form-data">
-        <dl>
-          <dt>ニックネーム</dt>
-          <dd>
-            <input type="text" name="name" size="35" maxlength="255" value="<?php print$_POST['name']; ?>"
-            >
-<!-- 
-            ♦htmlspecialcharsﾌｧﾝｸｼｮﾝ
-              htmlspecialcharsﾌｧﾝｸｼｮﾝを使うことで、HTMLﾀｸﾞの効果を打ち消して、イタズラのようなコードを無効化する
-
-            ♦ENT_QUOTES
-              「定義済みの定数」
-              これを指定することで
-                - 「"」が&quot;
-                - 「’」が&#039 or &apos;
-                - 「<」が &lt;
-                - 「>」が &gt;
-              以上に変換される
-
-              参考URL(https://www.leon-tec.co.jp/blog/yoshida/8214/)
---> 
-
-            <?php echo '<br>'; ?>
-            <?php var_dump($blank); ?>
-            <?php echo '<br>'; ?>
-
-            <!-- ニックネーム入力に関するエラー文 -->
-            <?php if($error['name']==='blank'):?>
-              <p class ="error">ニックネームを入力してください</p>
-            <?php endif; ?>
-            <?php if($error['name']==='max'):?>
-              <p class ="error">文字長すぎです</p>
-            <?php endif; ?>
-            <?php if($error['name']==='min'):?>
-              <p class ="error">文字短すぎです</p>
-            <?php endif; ?>
-
-
-            <?php var_dump ($error['name']);  ?>
-
-            
-          </dd>
-
-          <dt>メールアドレス</dt>
-          <dd>
-            <input type="text" name="email" size="35" maxlength="255"
-            value="<?php print(htmlspecialchars($_POST['email'],ENT_QUOTES)); ?>"
-            >
-            
-            <!-- ニックネーム入力に関するエラー文 -->
-            <?php if($error['email']==='blank'):?>
-              <p class ="error">メールアドレスを入力してください</p>
-            <?php endif; ?>
-
-            <?php if($error['email']==='check'):?>
-              <p class ="error">正しくメールアドレスが入力されていないようです</p>
-            <?php endif; ?>
-
-          </dd>
-
-          <dt>パスワード</dt>
-          <dd>
-            <input type="password" name="password"  size="10" maxlength="20" value="<?php print(htmlspecialchars($_POST['password'],ENT_QUOTES)); ?>">
-
-            <?php if($error['password']==='blank'):?>
-            <p class ="error">パスワードを入力してください</p>
-            <?php endif; ?>
-
-            <?php if($error['password']==='length'):?>
-            <p class ="error">パスワードは4文字以上で入力してください</p>
-            <?php endif; ?>
-    
-
-          </dd>
-
-          <dt>パスワード(確認)</dt>
-          <dd>
-            <input type="password" name="certify_password" size="10" maxlength="20" value="<?php print(htmlspecialchars($_POST['certify_password'],ENT_QUOTES)); ?>">
+        <div class="p-change-btn p-next-btn " >
+          <div class="p-modal__turnIcon">
+            <i class="fa fa-chevron-right fa-2x"></i>
+            <p>next</p>
+          </div>
           
-
-          </dd>
-          <?php if($error['certify_password'] ==='blank'): ?>
-              <p class ="error">確認パスワードを入力してください</p>
-            <?php endif; ?>
-
-            <?php if($_POST['certify_password'] !== $_POST['password']): ?>
-              <p class ="error">パスワードと確認パスワードが一致しません</p>
-            <?php endif;?>
-            
-
-        </dl>
-<!-- △△△△△△△△ -->
-
-        <div>
-          <input type="submit" value="入力内容を確認する" class="input-btn">
         </div>
-      </form>
+
+        <div class="p-alter-btn ">
+          <div class="p-modal__turnIcon">
+            <i class="fa fa-chevron-right fa-2x"></i>
+            <p>next</p>
+          </div>
+          
+        </div>
+      </div>
+    <!--inner  -->
+
+      <!-- #0 -->
+      <div class=" c-modal__body p-modal__body">
+
+        <h4 class="p-modal__body--title">#0 Greeting</h4>
+        <p>はじめまして。カツノリです<br>Twitter風webアプリ完成しました。よろしかったら、遊んでみてください。</p> 
+      </div>
+    
+      <!-- #1 -->
+      <div class="c-modal__body p-modal__body">
+        <h4 class="p-modal__body--title">個人情報取扱指針</h4>
+          <div class="p-modal__body--txtArea">
+          
+            <?php include('privacy&policy.php'); ?>
+          </div>
+          <div class = p-modal__body--checkBtn>
+            <input id="check" type="checkbox" >上記に同意する
+          </div>
+      </div>
+
+      <!-- #2 -->
+      <div class="c-modal__body  p-modal__body">
+        <h4 class="p-modal__body--title">#2 登録画面</h4>
+        <form action="" method="post" enctype="multipart/form-data">
+          <dl>
+            <dt>ニックネーム</dt>
+            <dd>
+              <input type="text" name="name" size="35" maxlength="255" value="<?php print$_POST['name']; ?>"
+              >
+              <!-- 
+                ♦htmlspecialcharsﾌｧﾝｸｼｮﾝ
+                htmlspecialcharsﾌｧﾝｸｼｮﾝを使うことで、HTMLﾀｸﾞの効果を打ち消して、イタズラのようなコードを無効化する
+
+                ♦ENT_QUOTES
+                「定義済みの定数」
+                これを指定することで
+                  - 「"」が&quot;
+                  - 「’」が&#039 or &apos;
+                  - 「<」が &lt;
+                  - 「>」が &gt;
+                以上に変換される
+
+                参考URL(https://www.leon-tec.co.jp/blog/yoshida/8214/)
+              --> 
+
+              
+              <!-- ニックネーム入力に関するエラー文 -->
+              <?php if($error['name']==='blank'):?>
+                <p class ="error">ニックネームを入力してください</p>
+              <?php endif; ?>
+              <?php if($error['name']==='max'):?>
+                <p class ="error">文字長すぎです</p>
+              <?php endif; ?>
+              <?php if($error['name']==='min'):?>
+                <p class ="error">文字短すぎです</p>
+              <?php endif; ?>
+
+
+              <?php var_dump ($error['name']);  ?>
+
+              
+            </dd>
+
+            <dt>メールアドレス</dt>
+            <dd>
+              <input type="text" name="email" size="35" maxlength="255"
+              value="<?php print(htmlspecialchars($_POST['email'],ENT_QUOTES)); ?>"
+              >
+              
+              <!-- ニックネーム入力に関するエラー文 -->
+              <?php if($error['email']==='blank'):?>
+                <p class ="error">メールアドレスを入力してください</p>
+              <?php endif; ?>
+
+              <?php if($error['email']==='check'):?>
+                <p class ="error">正しくメールアドレスが入力されていないようです</p>
+              <?php endif; ?>
+
+            </dd>
+
+            <dt>パスワード</dt>
+            <dd>
+              <input type="password" name="password"  size="10" maxlength="20" value="<?php print(htmlspecialchars($_POST['password'],ENT_QUOTES)); ?>">
+
+              <?php if($error['password']==='blank'):?>
+              <p class ="error">パスワードを入力してください</p>
+              <?php endif; ?>
+
+              <?php if($error['password']==='length'):?>
+              <p class ="error">パスワードは4文字以上で入力してください</p>
+              <?php endif; ?>
+            </dd>
+
+            <dt>パスワード(確認)</dt>
+            <dd>
+              <input type="password" name="certify_password" size="10" maxlength="20" value="<?php print(htmlspecialchars($_POST['certify_password'],ENT_QUOTES)); ?>">
+            </dd>
+              <?php if($error['certify_password'] ==='blank'): ?>
+                <p class ="error">確認パスワードを入力してください</p>
+              <?php endif; ?>
+
+              <?php if($_POST['certify_password'] !== $_POST['password']): ?>
+                <p class ="error">パスワードと確認パスワードが一致しません</p>
+              <?php endif;?>
+              
+
+          </dl>
+
+          <div>
+            <input type="submit" value="入力内容を確認する" class="input-btn">
+          </div>
+        </form>
+      </div>
+
+
     </div>
+
+
+
+
+
   </div>
 </div>
 
